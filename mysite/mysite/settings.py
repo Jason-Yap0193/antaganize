817a6b46929baa426ae7a1dce9094cffb917e4a2
JasonYap0193 <yap@mysoftinn.com>
2022-05-12 09:37:27 +0800

    Added files for heroku deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -132,3 +132,5 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+django_heroku.settings(locals())
